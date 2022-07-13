d52b2ed26cfaab7e3dad4bbf5ff0833e760b72cb
zhangchaochao <13861837044@163.com>
2022-07-13 18:02:09 +0800

    第一天上班

--- 智慧仓储/index.js
+++ 智慧仓储/index.js
@@ -1,6 +1,24 @@
 $(function () {
-    $("#sortType").on('click', '.sort', function () {
+    $(".condition").on('click', '.chooses .option', function () {
+        if ($(this).hasClass('active')) {
+            return false
+        }
         $(this).addClass('active').siblings().removeClass("active")
+    })
+    $("#sortType").on('click', '.sortName', function () {
+        if ($(this).parent().hasClass('active')) {
+            return false
+        }
+        $(this).parent().addClass('active').siblings().removeClass("active")
+        $(this).parent().find('.arrActive').removeClass('arrActive')
+        $(this).parent().find('.arrUp').addClass('arrActive')
+    })
+    $("#sortType").on('click', '.arrUp,.arrDown', function () {
+        if ($(this).hasClass('arrActive')) {
+            return false
+        }
+        $(this).addClass('arrActive').siblings().removeClass("arrActive")
+        $(this).parent().parent().addClass('active').siblings().removeClass("active")
     })
     $('#datetimepicker1').datetimepicker({
         format: 'YYYY-MM-DD'
@@ -21,6 +39,12 @@ $(function () {
             $("#indexSearch").hide()
         }
     })
+    $(".sub_nav").on('click', 'li', function () {
+        if ($(this).hasClass('current_nav')) {
+            return false
+        }
+        $(this).addClass('current_nav').siblings().removeClass("current_nav")
+    })
 })
 
 function goNeeds() {
@@ -29,8 +53,4 @@ function goNeeds() {
 
 function goNeedsDetail() {
     window.location.href = "./needs.html";
-}
-
-function goServiceDetail() {
-    window.location.href = "./service.html";
 }
