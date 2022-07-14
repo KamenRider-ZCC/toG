4ebe49a9826815dd1424477b2732829d52d2e8a0
zhangchaochao <13861837044@163.com>
2022-07-14 18:02:08 +0800

    二

--- 智慧仓储/index.js
+++ 智慧仓储/index.js
@@ -20,25 +20,6 @@ $(function () {
         $(this).addClass('arrActive').siblings().removeClass("arrActive")
         $(this).parent().parent().addClass('active').siblings().removeClass("active")
     })
-    $('#datetimepicker1').datetimepicker({
-        format: 'YYYY-MM-DD'
-    });
-    $('#datetimepicker2').datetimepicker({
-        format: 'YYYY-MM-DD'
-    });
-    $('#datetimepicker3').datetimepicker({
-        format: 'YYYY-MM-DD'
-    });
-    $('#datetimepicker4').datetimepicker({
-        format: 'YYYY-MM-DD'
-    });
-    $("#myTab").on('click', '.nav-link', function () {
-        if ($(this).attr('aria-controls') === 'all') {
-            $("#indexSearch").show()
-        } else {
-            $("#indexSearch").hide()
-        }
-    })
     $(".sub_nav").on('click', 'li', function () {
         if ($(this).hasClass('current_nav')) {
             return false
@@ -47,10 +28,29 @@ $(function () {
     })
 })
 
-function goNeeds() {
-    $('#needs-tab').tab('show')
+function goForm() {
+    window.location.href = "./form.html";
 }
 
 function goNeedsDetail() {
     window.location.href = "./needs.html";
+}
+// 百度地图API功能
+var map = new BMap.Map("map");
+var point = new BMap.Point(116.331398, 39.897445);
+map.centerAndZoom(point, 12);
+// 创建地址解析器实例
+var myGeo = new BMap.Geocoder();
+// 将地址解析结果显示在地图上,并调整地图视野
+function getAddress(address) {
+    myGeo.getPoint(address, function (point) {
+        if (point) {
+            map.addOverlay(new BMap.Marker(point));
+            setTimeout(() => {
+                map.centerAndZoom(point, 16);
+            }, 300)
+        } else {
+            alert("您选择地址没有解析到结果!");
+        }
+    });
 }
